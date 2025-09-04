5b856a09d02cbab5472770e1b6584743ca37f698
gvc19 <gaganvc19@gmail.com>
2025-09-04 23:33:20 +0530

    ep1ch4

--- namste react/App.js
+++ namste react/App.js
@@ -1,12 +1,28 @@
+{/* <div id="parent">
+    <div id="child">
+        <h1></h1>
+    </div>
+</div> */}
+
+
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from react");
+
+// const child = React.createElement("div", {id:"child"}, heading)
+// const parent = React.createElement("div", {id:"parent"}, child)
 
 
 
 
+const parent = React.createElement(
+    "div",
+     {id:"parent"},
+      React.createElement(
+        "div",
+         {id:"child"},
+          [React.createElement("h1", {key:1}, "im an h1 tag"),React.createElement("h2", {key:2}, "im an h2 tag")]))
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from react");
-
-console.log(heading)
+console.log(parent)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
